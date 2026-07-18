63581c689b7ac0346e5e5242003f9abdea3f119d
upgpeptilo <320790320+upgpeptilo@users.noreply.github.com>
2026-07-18 20:25:19 +0100

    Replace WhatsApp floating chat button with Smartsupp live chat

--- src/components/SiteChrome.tsx
+++ src/components/SiteChrome.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Script from "next/script";
 import Header from "./Header";
 import Footer from "./Footer";
 import FloatingActions from "./FloatingActions";
@@ -18,6 +19,18 @@ export default function SiteChrome({ children }: { children: React.ReactNode }) 
       <main className="flex-1">{children}</main>
       <Footer />
       <FloatingActions />
+      <Script id="smartsupp-chat" strategy="afterInteractive">
+        {`
+          var _smartsupp = _smartsupp || {};
+          _smartsupp.key = 'e0445f0470f0f56c8c2f6c84a22ab65db52c87fc';
+          window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+          })(document);
+        `}
+      </Script>
     </CartProvider>
   );
 }
