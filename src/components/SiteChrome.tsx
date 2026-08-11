927ede4556d904595df19872a9bd36a6f83ec1cd
upgpeptilo <320790320+upgpeptilo@users.noreply.github.com>
2026-08-11 14:26:15 +0100

    Update Jivo widget ID

--- src/components/SiteChrome.tsx
+++ src/components/SiteChrome.tsx
@@ -19,7 +19,7 @@ export default function SiteChrome({ children }: { children: React.ReactNode }) 
       <main className="flex-1">{children}</main>
       <Footer />
       <FloatingActions />
-      <Script src="//code.jivosite.com/widget/AnwYW4JENR" strategy="afterInteractive" />
+      <Script src="//code.jivosite.com/widget/OpXo9EL2wk" strategy="afterInteractive" />
     </CartProvider>
   );
 }
