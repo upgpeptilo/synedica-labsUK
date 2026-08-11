97daa4c1abf8b7139a6b97074719c5e0b3aff4b1
upgpeptilo <320790320+upgpeptilo@users.noreply.github.com>
2026-08-11 14:10:34 +0100

    Replace Smartsupp live chat with Jivo widget

--- src/components/SiteChrome.tsx
+++ src/components/SiteChrome.tsx
@@ -19,18 +19,7 @@ export default function SiteChrome({ children }: { children: React.ReactNode }) 
       <main className="flex-1">{children}</main>
       <Footer />
       <FloatingActions />
-      <Script id="smartsupp-chat" strategy="afterInteractive">
-        {`
-          var _smartsupp = _smartsupp || {};
-          _smartsupp.key = 'e0445f0470f0f56c8c2f6c84a22ab65db52c87fc';
-          window.smartsupp||(function(d) {
-            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-            c.type='text/javascript';c.charset='utf-8';c.async=true;
-            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-          })(document);
-        `}
-      </Script>
+      <Script src="//code.jivosite.com/widget/9UyLTP3NKm" strategy="afterInteractive" />
     </CartProvider>
   );
 }
